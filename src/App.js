60dabe7cad69b2b98da04d3d898f61c88844202a
Prabhat-git-99 <prabatwork3939@gmail.com>
2020-09-27 09:41:52 +0530

    Pages and Routing Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './shared/pages/Header';
+import Footer from './shared/pages/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Info from './pages/Info';
+import Error from './shared/pages/Error';
 import './App.css';
 
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path = '/' exact component = {Home} />
+          <Route path = '/about' exact component = {About} />
+          <Route path = '/contact' exact component = {Contact} />
+          <Route path = '/Info' exact component = {Info} />
+          <Route component = {Error} />
+        </Switch>
+        <Footer />
+      </Router>
     </>
   )
 }
-
 
 export default App;
