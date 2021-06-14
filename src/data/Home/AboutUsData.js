509a2a4df8d13cfd5504db7aa4a57fb5afe198fc
amitky29 <57131871+amitky29@users.noreply.github.com>
2021-06-14 20:02:17 +0530

    updated

--- src/data/Home/AboutUsData.js
+++ src/data/Home/AboutUsData.js
@@ -5,29 +5,29 @@ import b3 from '../../images/b3.jpg';
 const AboutUsData = [
     {
         img: gf1,
-        Name: 'Prajakta Nigade',
+        Name: 'Nikita Paytale',
         info: 'Frontend Developer, ML enthusiast. Contact me by clicking on the link',
         linkedin: 'https://www.google.com',
         fb: 'https://www.fb.com'
     },
     {
         img: b3,
-        Name: 'Rishabh Shukla',
-        info: 'Backend developer, worked on MongoDb, SQL, Node etc. ',
-        linkedin: 'https://www.google.com',
-        fb: 'https://www.fb.com'
-    },
-    {
-        img: gf1,
-        Name: 'Shravani Poman',
-        info: 'Frontend developer, React lover and equiped with SEO and SMM. Check out my profile!',
+        Name: 'Hrishikesh Kanojia',
+        info: ' Backend developer ',
         linkedin: 'https://www.google.com',
         fb: 'https://www.fb.com'
     },
     {
         img: b3,
-        Name: 'Prabhat Singh',
-        info: 'having 2 year of experience in Full stack development and new at ML and NLP.',
+        Name: 'Swapnil Patil',
+        info: ' Frontend developer. Check out my profile! ',
+        linkedin: 'https://www.google.com',
+        fb: 'https://www.fb.com'
+    },
+    {
+        img: b3,
+        Name: 'Amit Kumar Yadav',
+        info: ' New at ML and NLP ',
         linkedin: 'https://www.google.com',
         fb: 'https://www.fb.com'
     },
